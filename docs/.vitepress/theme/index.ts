7d2b41ba28806f9a63342cc49961c26a6729f44e
narumi <yan.jianhui147@gmail.com>
2024-05-03 06:01:16 +0800

    Support zoom image

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,28 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
+import { h, nextTick, onMounted, watch } from 'vue'
+import { useRoute, type Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import mediumZoom from 'medium-zoom'
 // import AdSense from '../../components/AdSense.vue'
 
 export default {
   extends: DefaultTheme,
+  setup() {
+    // https://github.com/vuejs/vitepress/issues/854
+    const route = useRoute()
+    const initZoom = () => {
+      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' });
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
+  },
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // 'aside-ads-before': () => h(AdSense),
